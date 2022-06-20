f1c159a9a15c57a0a77f668764c0a4fbf6430402
gaurav61194 <GAURAV.ANBHORE@CAPGEMINI>
2022-06-20 15:49:59 +0530

    push

---
+++ SpringBootWebDemo/src/main/java/com/controler/HomeController.java
@@ -0,0 +1,19 @@
+package com.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path="api")
+public class HomeController 
+{
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello()
+	{
+		System.out.println("Welcome to Spring Web");
+		return "Welcome to Spring Web";
+	}
+
+}
